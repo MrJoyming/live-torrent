fae5a8eda0b56475ac3f9d70465a70331b5cf870
Ming King <mingking@MingdeMacBook-Pro.local>
2020-10-13 11:23:05 +0800

    11

---
+++ src/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container>
+  <el-header><Header></Header></el-header>
+  <el-main><Mian></Mian></el-main>
+  <el-footer><Footer></Footer></el-footer>
+</el-container>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header'
+import Mian from './components/mian'
+import Footer from './components/footer'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Mian,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './index.vue'
 
 createApp(App).mount('#app')
